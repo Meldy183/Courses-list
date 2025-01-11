f1c83e47ae22eba3f0cfd64e3ce45e0daa2fe86a
Fyodor Markin <feduard2006@gmail.com>
2025-01-11 21:27:59 +0300

    Final version

--- app/src/main/java/com/example/courses/MainActivity.kt
+++ app/src/main/java/com/example/courses/MainActivity.kt
@@ -1,42 +1,34 @@
 package com.example.courses
-
-import android.icu.number.Scale
 import android.os.Bundle
-import android.provider.CalendarContract.Colors
-import android.text.style.BackgroundColorSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.sharp.Menu
-import androidx.compose.material.icons.sharp.Search
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.courses.data.DataSource
 import com.example.courses.topic.Topic
 import com.example.courses.ui.theme.CoursesListTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +36,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CoursesListTheme {
-
+                App()
             }
         }
     }
@@ -93,6 +85,29 @@ fun DrawTopic(topic: Topic, modifier: Modifier = Modifier) {
     }
 }
 
+
+@Composable
+fun DrawList(list: List<Topic>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.small)),
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.small)),
+        modifier = modifier
+    ) {
+        items(list) { topic ->
+            DrawTopic(topic)
+        }
+    }
+}
+
+@Composable
+fun App() {
+    val listOfTopic = DataSource.topics
+    DrawList(
+        listOfTopic,
+        modifier = Modifier.padding(dimensionResource(R.dimen.small))
+    )
+}
 
 @Preview
 @Composable
